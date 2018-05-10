599760ff4937eb1521b7df37b9bb8c0cf4a3537f
tom523 <358777330@qq.com>
2018-05-10 17:32:28 +0800

    llll

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -20,9 +20,17 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': '',                            # 你的数据库user
-        'PASSWORD': '',                        # 你的数据库password
-        'HOST': '',                   		   # 数据库HOST
+        'USER': 'root',                        # 你的数据库user
+        'PASSWORD': '123456',                        # 你的数据库password
+        'HOST': '192.168.200.11',                   # 开发的时候，使用localhost
         'PORT': '3306',                        # 默认3306
+    },
+    'dicos_menu': {
+        'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
+        'NAME': 'test_dicos_menu',  # 数据库名 (默认与APP_ID相同)
+        'USER': 'admin',  # 你的数据库user
+        'PASSWORD': '123456',  # 你的数据库password
+        'HOST': '192.168.200.176',  # 开发的时候，使用localhost
+        'PORT': '3306',  # 默认3306
     },
 }
