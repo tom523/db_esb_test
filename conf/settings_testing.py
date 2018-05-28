aea3d5711174c6c2c1782165d031d048757359b9
tom523 <358777330@qq.com>
2018-05-28 16:30:43 +0800

    llll

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -20,9 +20,9 @@ DATABASES = {
     'default': {   # 存储菜单版本原始数据
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': "db_esb_test",                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'admin',                        # 你的数据库user
-        'PASSWORD': '123456',                        # 你的数据库password
-        'HOST': '192.168.200.176',                   # 开发的时候，使用localhost
+        'USER': 'root',                        # 你的数据库user
+        'PASSWORD': 'newpwd',                        # 你的数据库password
+        'HOST': '192.168.200.11',                   # 开发的时候，使用localhost
         'PORT': '3306',                        # 默认3306
     },
     'dicos_menu': {  #  映射dicos生产数据库
@@ -37,7 +37,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': 'store_info',  # 数据库名 (默认与APP_ID相同)
         'USER': 'root',  # 你的数据库user
-        'PASSWORD': '123456',  # 你的数据库password
+        'PASSWORD': 'newpwd',  # 你的数据库password
         'HOST': '192.168.200.11',  # 开发的时候，使用localhost
         'PORT': '3306',  # 默认3306
     },
